ec43b8fd0127741f2017576401a7ffe6c601ae7b
LucaCapolupo <105062046+LucaCapolupo@users.noreply.github.com>
2023-05-04 14:04:49 +0200

    [feat] removed blazor project and recreated it with mudblazor template

--- ITS.PW2.UserWebApplication/Data/WeatherForecast.cs
+++ ITS.PW2.UserWebApplication/Data/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace ITS.PW2.UserWebApplication.Data
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
--- ITS.PW2.UserWebApplication/Program.cs
+++ ITS.PW2.UserWebApplication/Program.cs
@@ -1,13 +1,18 @@
 using ITS.PW2.UserWebApplication.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
